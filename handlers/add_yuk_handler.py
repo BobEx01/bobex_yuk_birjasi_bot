b9e59727387a9337c0ed1dbde2735b944bec4372
BobEx01 <boburi9000@mail.ru>
2025-07-13 06:00:02 +0500

    Update add_yuk_handler.py

--- handlers/add_yuk_handler.py
+++ handlers/add_yuk_handler.py
@@ -1,1 +1,7 @@
-def register(dp): pass  # Add your handler logic here
+from aiogram import types, Dispatcher
+
+async def add_yuk(message: types.Message):
+    await message.answer("Yuk qo‘shish funksiyasi ishlamoqda...")
+
+def register_handlers(dp: Dispatcher):
+    dp.register_message_handler(add_yuk, commands=['add_yuk'])
